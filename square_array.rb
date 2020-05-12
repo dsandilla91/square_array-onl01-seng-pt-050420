c78c751f8888593c05886b5138b84b9a4f1cbc12
Deborah Santillan <s.debb@yahoo.com>
2020-05-12 22:04:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
+require "pry"
+
 def square_array(array)
   square_array = []
   
   array.each do |number|
-    puts number 
-    puts square_array
+    binding.pry 
    square_array << number**2
   end
   return square_array
